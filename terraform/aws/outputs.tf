1e4ff76e33727b4b6dd96f499b8c4e8120bf6e0c
Wahid Sholihul Fikri <wahidsf1905@gmail.com>
2026-03-04 23:53:10 +0700

    Add docker-compose.yml and .gitignore

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,30 +1,11 @@
-resource "aws_ssm_document" "add_ssh_key" {
-  name          = "AddUbuntuSSHKey"
-  document_type = "Command"
-
-  content = jsonencode({
-    schemaVersion = "2.2"
-    description   = "Add SSH public key to ubuntu authorized_keys"
-    mainSteps = [{
-      action = "aws:runShellScript"
-      name   = "addKey"
-      inputs = {
-        runCommand = [
-          "install -d -m 700 /home/ubuntu/.ssh",
-          "echo '${var.ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys",
-          "chown -R ubuntu:ubuntu /home/ubuntu/.ssh",
-          "chmod 600 /home/ubuntu/.ssh/authorized_keys"
-        ]
-      }
-    }]
-  })
+output "instance_id" {
+  value = aws_instance.this.id
 }
 
-resource "aws_ssm_association" "add_ssh_key" {
-  name = aws_ssm_document.add_ssh_key.name
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
 
-  targets {
-    key    = "InstanceIds"
-    values = [aws_instance.this.id]
-  }
+output "ssm_connect" {
+  value = "aws ssm start-session --target ${aws_instance.this.id}"
 }
